d05551f8575857fb8f50df4eddf90da4a24a539e
leeprince <leeprince@foxmail.com>
2020-10-26 21:14:09 +0800

    doc: 配置说明 ｜ config 读取组件包配置说明

--- src/Wap/Member/Providers/MemberServiceProvider.php
+++ src/Wap/Member/Providers/MemberServiceProvider.php
@@ -83,31 +83,10 @@ class MemberServiceProvider extends ServiceProvider
          */
         /**
          * 即读取配置文件的方式有：
-         *     1. 通过 {文件名.配置项} 的方式
+         *     1. 使用$this->publishes([__DIR__ . '/Config' => config_path()], $groups = null);发布配置后，通过 {文件名.配置项} 的方式
          *     2. 通过 {mergeConfigFrom后key != 文件名的键.配置项} 的方式
          *     3. 通过{Arr::dot()合并后的键.配置项}读取
-         */
-        /**
-        "wap" => array:1 [▼
-          "member" => array:1 [▼
-            "auth" => array:4 [▼
-              "controller" => "LeePrince\LaravelWechatShop\Wap\Member\Http\Controllers\AuthorizationController"
-              "guard" => "member"
-              "guards" => array:1 [▼
-                "member" => array:2 [▼
-                  "driver" => "session"
-                  "provider" => "member"
-                ]
-              ]
-              "providers" => array:1 [▼
-                "member" => array:2 [▼
-                  "driver" => "eloquent"
-                  "model" => "LeePrince\LaravelWechatShop\Wap\Member\Models\User"
-                ]
-              ]
-            ]
-          ]
-        ]
+         *      注意：因为发布文件之后，外部文件配置可能会修改，以下第1和第2种方式都不能读取到新的修改。所以允许修改的部分就通过第1中去读取，不允许修改的部分使用第1和第2种都行
          */
         $this->mergeConfigFrom(__DIR__ . "/../Config/leeprince-member.php", 'wap.member');
     }
